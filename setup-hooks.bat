647bb57beaebabb5ba7cfd15a55dfc457bee0b89
Nguyễn Hải Hưng <156663233+NguyenHaiHung0510@users.noreply.github.com>
2026-02-23 15:12:25 +0700

    Hoàn tất file tool cấu hình

--- setup-hooks.bat
+++ setup-hooks.bat
@@ -1,24 +1,25 @@
 @echo off
 echo ===================================================
-echo   Cai dat Moi truong DevSecOps - microShop Project
+echo Cai dat Moi truong DevSecOps - GitLeaks + PMD + SpotBugs
+echo Chiu trach nhiem tool: Nguyen Hai Hung - Hoc vien Cong nghe Buu Chinh Vien Thong
 echo ===================================================
 
 :: 1. Kiểm tra Python
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo [LOI] Python chua duoc cai dat. Vui long cai dat Python truoc!
+    echo [LOI] Python chua duoc cai dat. Vui long cai dat Python!
     pause
     exit /b
 )
 
 :: 2. Cài đặt pre-commit
-echo [INFO] Kiem tra cap nhat pip truoc khi cai dat package
+echo [INFO] Kiem tra cap nhat pip
 python -m pip install --upgrade pip
-echo [INFO] Dang cai dat pre-commit...
+echo [INFO] Cai dat pre-commit
 python -m pip install pre-commit
 
 :: 3. Kích hoạt hooks vào thư mục .git
-echo [INFO] Dang tich hop hooks vao Git...
+echo [INFO] Tich hop hooks vao Git...
 python -m pre_commit install
 
 :: 4. Cài đặt các thư viện phụ thuộc (GitLeaks, etc)
@@ -26,6 +27,6 @@ echo [INFO] Dang cap nhat moi truong pre-commit...
 python -m pre_commit autoupdate
 
 echo ===================================================
-echo   HOAN TAT! He thong da san sang de commit an toan.
+echo HOAN TAT! He thong da cau hinh thanh cong!
 echo ===================================================
 pause

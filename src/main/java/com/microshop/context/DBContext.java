800f5874e71b9b6095a0ecc014d2939437582d97
Nguyễn Hải Hưng <156663233+NguyenHaiHung0510@users.noreply.github.com>
2026-02-25 16:38:36 +0700

    Test trigger Pha 1

--- src/main/java/com/microshop/context/DBContext.java
+++ src/main/java/com/microshop/context/DBContext.java
@@ -13,7 +13,6 @@ public class DBContext {
     // Cái này là tài khoản root của MySQL server trên máy của <Hưng>
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "hung";
-    // Hello
     
     private static HikariDataSource dataSource;
 
